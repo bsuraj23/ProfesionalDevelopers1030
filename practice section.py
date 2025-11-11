1edb99d00b8409bbb35ba2aece9bff6bd0a80dde
Dontharaboina Ramakrishna <ramakrishnadontharaboina4@gmail.com>
2025-11-11 11:48:15 +0530

    Update practice section.py
    update

--- practice section.py
+++ practice section.py
@@ -1,61 +1,61 @@
 # # different examoles in python
 # # integer variable
-# age=25
-# print(age)
+ age=25
+ print(age)
 # # integer inside integer
-# age=(25,(30))
-# print(age)
+ age=(25,(30))
+ print(age)
 # # combination of int and string
-# age,name=23,"akanksha"
-# print(age,name)
+ age,name=23,"akanksha"
+ print(age,name)
 
 
 
 # # string variable
-# name="veeraswamy"
-# print(name)
+ name="veeraswamy"
+ print(name)
 # # string 2
-# name="veeraswamy,swathi,abhi,vedhansh" 
-# print(name)
+ name="veeraswamy,swathi,abhi,vedhansh" 
+ print(name)
 # # string inside string
-# name=("ram",("akanksha"))
-# print(name)
+ name=("ram",("akanksha"))
+ print(name)
 # # combination of int and variable
-# name,age="ram",23
-# print(name,age)
+ name,age="ram",23
+ print(name,age)
 
 
 
 # #float variable
-# price=88.88
-# print(price)
+ price=88.88
+ print(price)
 # # float inside float
-# height=(5.5,(88.8))
-# print(height)
+ height=(5.5,(88.8))
+ print(height)
 # # combination of int,string and float
-# price,age,name=(22.2,(23),("ram"))
-# print(price,age,name)
+ price,age,name=(22.2,(23),("ram"))
+ print(price,age,name)
 
 
 
 # # boolean variable
-# is_student=True
-# print(is_student)
+ is_student=True
+ print(is_student)
 # # boolean inside boolean
-# is_student=(True,(False))
-# print(is_student)
+ is_student=(True,(False))
+ print(is_student)
 # # combination of int,string,float,boolean
-# age,name,height,is_student=((23),("ram"),(5.5),(True))
-# print(age,name,height,is_student)
+ age,name,height,is_student=((23),("ram"),(5.5),(True))
+ print(age,name,height,is_student)
 
 
 
 
 # # m,ultiple variables
-# x,y,z=1,2,3
-# print(x,y,z)
-# name1,name2,name3="ram","malli","karthik"
-# print(name1,name2,name3)
+ x,y,z=1,2,3
+ print(x,y,z)
+ name1,name2,name3="ram","malli","karthik"
+ print(name1,name2,name3)
 
 
 
@@ -63,30 +63,30 @@
 
 # # variable reassignment
 # #  a variables values vcan  e changed in variable reassignment
-# city="hyderabad"
-# print(city)
-# print(city is "hyderabad")
-# print(city is not "hyderabad")
-# city="vijayavada"
-# print(city)
-# print(city is "vijayavada")
-# print(city is not "vijayavada")
+ city="hyderabad"
+ print(city)
+ print(city is "hyderabad")
+ print(city is not "hyderabad")
+ city="vijayavada"
+ print(city)
+ print(city is "vijayavada")
+ print(city is not "vijayavada")
 
 
 
 
 # # list variable
-# fruits=["apple","banana","cherry"]
-# print(fruits)
+ fruits=["apple","banana","cherry"]
+ print(fruits)
 # # [] brackets stores a list
 
 
 
 
 # # combining variables
-# name="akanksha"
-# age=23
-# print("my name is",name,"and i am",age,"years old.")
+ name="akanksha"
+ age=23
+ print("my name is",name,"and i am",age,"years old.")
 
 
 
@@ -97,20 +97,20 @@
 
 
 # strings concepts
-# name1="ramakrishna"
-# name2="akanksha"
-# name3='''this is a 
-# multiline string.'''
+ name1="ramakrishna"
+ name2="akanksha"
+ name3='''this is a 
+ multiline string.'''
 
-# print(name1)
-# print(name2)
-# print(name3)
+ print(name1)
+ print(name2)
+ print(name3)
 
 # # string indexing
-# text="yaswanth"
-# print(text[0])    
-# print(text[5])
-# print(text[-1])
+ text="yaswanth"
+ print(text[0])    
+ print(text[5])
+ print(text[-1])
 
 
 # palindrome
