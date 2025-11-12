9dd7b39744463d6a71f88773a8e91220fbf166a8
Dontharaboina Ramakrishna <ramakrishnadontharaboina4@gmail.com>
2025-11-12 22:43:51 +0530

    Add files via upload

---
+++ built in function.py
@@ -0,0 +1,8 @@
+# built in function already provides python function in program which type requires
+numbers=[1,2,3,4,5,6]
+print("numbers:",numbers)
+print("total:",sum(numbers))
+print("maximum:",max(numbers))
+print("minimum:",min(numbers))
+print("sorted:",sorted(numbers))
+print("length:",len(numbers))
