a88eb51598973f6c6d5523c33791ec9cf79727bf
priyadharshini83744-pixel <priyadharshini83744@gmail.com>
2025-11-18 12:13:25 +0530

    Add files via upload

--- string.py
+++ string.py
@@ -1,2 +1,27 @@
 s = "Hello World"
 print(len(s))   
+
+
+
+s = "HeLLo"
+print(s.lower())   
+
+
+
+s = "Hello"
+print(s.upper())   
+
+
+
+
+s = "welcome to python"
+print(s.title()) 
+
+
+s = "   Python   "
+print(s.strip())   
+
+
+
+s = "I love Java"
+print(s.replace("Java", "Python"))  
