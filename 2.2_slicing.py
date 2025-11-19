6e488a1535a5d560f01f287ed009ceae12656adc
polagoni ajay kumar <polagoniajaykumar@polagonis-MacBook-Air.local>
2025-11-19 12:31:43 +0530

    deleted

--- 2.2_slicing.py
+++
@@ -1,9 +0,0 @@
-# String Slicing
-# used to get part of a string using [start:end:step]
-
-text = "Python Programming"
-print(text[0:6]) # Python
-print(text[7:]) # last 7 letters (Pragraming)
-print(text[:6]) # first 6 latters (Python)
-print(text[0:10:2]) 
-print(text[::2])
--- anime.py
+++
@@ -1,1 +0,0 @@
-print("obito Uchiha")
