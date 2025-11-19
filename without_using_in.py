f496d5e683da2253af31e2bf2d89cecfedd4f6d0
G.Venkata sasi kumar <sasiyadav667@gmail.com>
2025-11-19 11:42:24 +0530

    Add files via upload

---
+++ without_using_in.py
@@ -0,0 +1,19 @@
+fruit="mango"
+thing="bat"
+print('g' in fruit)
+print('b' in thing)
+if 'f' in fruit:
+    print("f is present in fruit")
+    if 'a' in thing:
+        print("a is present in thing")
+
+
+
+fruit="mango"
+thing="bat"
+if 'f' in fruit:
+    print("f is present in fruit")
+else:
+    print("f is not present in fruit")
+    if'a' in thing:
+        print("a is present in thing")   
