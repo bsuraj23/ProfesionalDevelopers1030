308d41ba04a4e3390eaa73fd74961241027491e3
Akhila1014 <akhilamajji0@gmail.com>
2025-12-03 22:16:49 +0530

    Add files via upload
    Multiples of 3 or 5

---
+++ multiples of 3 or 5.py
@@ -0,0 +1,11 @@
+limit = 100
+
+print("Multiples of 3 :")
+for i in range(1, limit + 1):
+    if i % 3 == 0:
+        print(i, end=" ")
+
+print("\nMultiples of 5 :")
+for l in range(1,limit + 1):
+     if l % 5 == 0:
+         print(l, end=" ")        
