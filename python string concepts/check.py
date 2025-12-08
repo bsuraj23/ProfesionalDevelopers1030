701389c11020f9f3c855e0d54dc1f4ec755a6963
KRATOS369 <adityakanchoji@gmail.com>
2025-12-08 18:28:51 +0530

    Save local changes

--- python string concepts/check.py
+++ python string concepts/check.py
@@ -1,3 +1,49 @@
+
+s = "Darksiders2"
+print("dar" in s)
+print("rs2" in s)
+print("2" in s)
+print("     ")
+
+
+str="God of war"
+print(str.startswith("God"))
+print(str.endswith("war"))
+print("of " in str)
+print("")
+
+#comparision
+A="CarlJohnson"
+B="Trevor"
+print(A==B)
+print(A<B)
+print(A>B)
+print("")
+
+#Counting
+s="Froza Horizon"
+print(s.count("H"))
+print("")
+
+#finding
+S = "Red Dead Redemption"
+print(S.find("Dead"))
+print(S.find("Redemption"))
+print("")
+
+#format string
+
+name = "yesh"
+age = 22
+height = 5.7
+print("my name is {} and my age is {} and height is {}".format(name, age, height))
+print("my name is {N} and my age is {A} and height is {H}".format(N=name, A=age, H=height))
+print(f"my name is {name}, my age is {age}, height is {hight}")
+print("")
+
+Number = 1224.567889
+print("format number is {:.2f}".formaformat number is {}".format(Number))
+=======
 s = "Darksiders2"
 print("dar" in s)
 print("rs2" in s)
@@ -43,4 +89,9 @@ print("")
 Number = 1224.567889
 print("format number is {:.2f}".format(Number))
 print("format number is {}".format(Number))
-print("format number is {:.8f}".format(Number))
+>>>>>>> 6d414cc (new code concepts updated)
+print("format number is {:.8f}".format(Number))lue is",c)
+print(type(c))
+
+print("d value is",d)
+print(type(d))
