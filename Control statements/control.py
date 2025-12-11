4b94b3381112fcd8fc4fd75792d9c2a943cc9c59
KRATOS369 <adityakanchoji@gmail.com>
2025-12-11 18:39:13 +0530

    change

--- Control statements/control.py
+++ Control statements/control.py
@@ -6,3 +6,16 @@ elif a == 6:
     print("value is equal",a)
 else:
     print("value is not equal",a)
+
+# if statement example 2
+
+citizen = "Indian"
+age = 18
+if (citizen == "African"):
+    if (age>=18):
+        print("Citizen")
+    else:
+        print("Not Greater than 18")
+else:
+    print("not a citizen")
+print("end of the code")
